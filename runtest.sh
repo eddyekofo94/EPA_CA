29ef82dee5a0597af149d037a4eecd0d0b317cc1
Eddy Ekofo <contact@eddyekofo.com>
2017-12-07 09:13:40 +0000

    Got the contrainst working 1<=N <=50

--- runtest.sh
+++ runtest.sh
@@ -2,9 +2,25 @@
 
 echo "runtest.sh is running..."
 
-for i in {1..20}
-do
-    echo $i
-    # do something here
-done
+#   Variables
+echo enter number of users: $numberOfUsers
 
+# loadtest=`./loadtest`
+read numberOfUsers
+
+if [[ $numberOfUsers -ge 1 && $numberOfUsers -le 50 ]] ; then 
+  echo "good"
+else
+  echo "bad"
+fi
+
+
+#./loadtest $numberOfUsers
+
+
+
+# for i in {1..20}
+# do
+  #  echo $i
+    # do something here
+# done
