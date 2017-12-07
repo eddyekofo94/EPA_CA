7522ab02b4da8fb8451133db0d244e97e7825d2f
Eddy Ekofo <contact@eddyekofo.com>
2017-12-07 12:38:44 +0000

    I think the script is working

--- runtest.sh
+++ runtest.sh
@@ -4,14 +4,26 @@ echo "runtest.sh is running..."
 
 #   Variables
 echo enter number of users: $numberOfUsers
-
+resultFile="./result.dat"
 # loadtest=`./loadtest`
 read numberOfUsers
 
 if [[ $numberOfUsers -ge 1 && $numberOfUsers -le 50 ]] ; then 
-  echo "good"
+     
+    echo "Accepted input"
+    users=$numberOfUsers
+    for i in {1..50}
+     do
+         if [ -e "$resultFile" ]; then
+         ./loadtest $users& >> `mpstat -o JSON >> test_result.json`
+
+             #./loadtest $users& >> `mpstat -o JSON >> test_result.json`
+        fi    
+ done
+
+     echo "All done"
 else
-  echo "bad"
+  echo "Bad inputs, try again or exit"
 fi
 
 
