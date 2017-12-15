928c11c38b513de41e5232fb7e0c8c19dab45029
Eddy Ekofo <contact@eddyekofo.com>
2017-12-15 18:39:00 +0000

    DONE: runtest.sh  TODO: Graph

--- runtest.sh
+++ runtest.sh
@@ -3,23 +3,36 @@
 echo "runtest.sh is running..."
 
 #   Variables
-echo enter number of users: $numberOfUsers
-resultFile="./result.dat"
+# echo enter number of users: $numberOfUsers
+    #for i in {1..$users}TH
+#deleteSynFile="rm synthetic.dat"
+resultFile="result.dat"
 # loadtest=`./loadtest`
-read numberOfUsers
+# read numberOfUsers
 
-if [[ $numberOfUsers -ge 1 && $numberOfUsers -le 50 ]] ; then 
-     
+if [[ $1 -ge 1 && $1 -le 150 ]] ; then 
     echo "Accepted input"
-    users=$numberOfUsers
-    for i in {1..50}
+    rm "synthetic.dat"
+    rm $resultFile
+    touch $resultFile
+    
+    echo CO$'\t'N$'\t'idle >> $resultFile 
+    users=$1
+    seconds=$2
+    for((i=1;i<=$1;i++))
      do
-         if [ -e "$resultFile" ]; then
-         ./loadtest $users& >> `mpstat -o JSON >> test_result.json`
-
-             #./loadtest $users& >> `mpstat -o JSON >> test_result.json`
-        fi    
- done
+        timeout $seconds ./loadtest $i &
+         # printf " %-20s %-20s %-20s\n " CO\ N\ Idle
+         #wc -l < synthetic.dat >> "$resultFile"
+         CO=`wc -l < synthetic.dat`
+         #echo $CO
+        #echo $i >> "$resultFile"
+        N="$i"
+         #mpstat -o JSON | jq -r '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle' >> $resultFile
+        idle=`mpstat 10 1 -o JSON | jq -r '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
+        echo $idle
+        echo $CO$'\t'$i$'\t'$idle >> $resultFile
+     done
 
      echo "All done"
 else
