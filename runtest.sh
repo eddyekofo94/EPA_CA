9c92cbf282798ef894f98ca3f3a0b300e09fab54
Eddy Ekofo <contact@eddyekofo.com>
2017-12-18 01:59:21 +0000

    Used 50 transactions

--- runtest.sh
+++ runtest.sh
@@ -10,17 +10,18 @@ echo "Accepted input"
 rm "synthetic.dat"
 rm $resultFile
 touch $resultFile
+touch "synthetic.dat"
 
 echo CO$'\t'N$'\t'idle >> $resultFile #writes the heading on each column 
 users=$1  # inputs from the user
 seconds=$2  #seconds for the timeout to kill
-for((i=1;i<=$1;i++))
+for((i=1;i<=50;i++))
 do
     timeout 10 ./loadtest $i &  # timout is a plugin which automatically closes processes
-    CO=`wc -l < synthetic.dat` how many lines 
+    CO=`wc -l < synthetic.dat` #how many lines 
     N="$i"
     idle=`mpstat 10 1 -o JSON | jq -r '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
-    echo $idle
+    echo $i$'\t'$idle
     echo $CO$'\t'$i$'\t'$idle >> $resultFile
 done
 
